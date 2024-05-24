60423866b04716b8a6def201701c043c84324f9b
NiekBrouwer98 <niek.brouwer@outlook.com>
2024-05-24 10:02:13 +0200

    additional plots

--- process_data.py
+++ process_data.py
@@ -15,8 +15,9 @@ adata_orig.obs = adata_orig.obs.rename(columns={'n.umi':'n_counts'})
 adata_orig.var = adata_orig.var.rename(columns={'Ensemble Id':'ensembl_id'})
 #How do we assign labels?
 adata_orig.obs['treated_label'] = np.where((adata_orig.obs['dose'] == '0.0'), 'Untreated', adata_orig.obs['drug'])
+adata_orig.obs['treated_label'] = np.where((adata_orig.obs['dose'] == '10.0') | (adata_orig.obs['dose'] == '0.0'), adata_orig.obs['treated_label'],'Other' )
 adata_orig.var['ensembl_id']  = [s.split('.')[0] for s in adata_orig.var['ensembl_id']]
 
-adata_orig.write_h5ad('C:/Users/nbrouwer1/Documents/VS_projects/prepare_data/input_files/sciplex_processed/sciplex2_processed2.h5ad')
+adata_orig.write_h5ad('C:/Users/nbrouwer1/Documents/VS_projects/prepare_data/input_files/sciplex_processed/sciplex2_processed3.h5ad')
 
 print('DATA PROCESSED')
